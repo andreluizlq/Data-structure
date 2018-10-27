3c8cea8f8bc4ec86b6c32554b2b879eab328df7a
andreluizlq <38213016+andreluizlq@users.noreply.github.com>
2018-10-26 22:48:51 -0300

    Create Questão1.cpp

---
+++ Teorica/Questão1.cpp
@@ -0,0 +1,6 @@
+//Questão 1)
+//Um vetor para listar os nomes dos alunos de algum período que estão matriculados em alguma matéria. 
+string nome[n];
+
+//Uma matriz para guardar as notas de cada unidade dos alunos.
+float notas[n][m];
